d626582c44d19e96505b138725c4a5c3693dba4b
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2023-12-18 12:09:04 +0530

    env tracking added to all emit-expr

--- compiler.scm
+++ compiler.scm
@@ -6,6 +6,20 @@
 (define null-val #b00101111)
 (define word-size 8)
 
+; cases for compiler
+(define (compile-program x)
+    ; initialize stack index - word-size so as not to 
+    ; overwrite return address
+    (emit-expr (- word-size) '() x)
+    (emit "ret"))
+
+(define (emit-expr si env expr)
+    (cond
+        ((immediate? expr) (emit-immediate expr))
+        ((primcall? expr) (emit-primcall si env expr))
+        (else (error "syntax error"))))
+
+; immediate constants
 (define (immediate? x)
     ;check if x is one of immediate type
     (or (integer? x) (char? x) (boolean? x) (null? x)))
@@ -25,17 +39,19 @@
         ; empty list
         ((null? x) null-val)
         (else (error "no immediate representation for" x))))
+(define (emit-immediate expr)
+    (emit "movl $~a, %eax" (immediate-rep expr)))
 
 ; infra for primitives 
 (define-syntax define-primitive
     (syntax-rules ()
-        ((_ (prim-name si arg* ...) b b* ...)
+        ((_ (prim-name si env arg* ...) b b* ...)
             (begin
                 (set-symbol-property! 'prim-name '*is-prim* #t)
                 (set-symbol-property! 'prim-name '*arg-count* 
                     (length '(arg* ...)))
                 (set-symbol-property! 'prim-name '*emitter*
-                    (lambda (si arg* ...) b b* ...))))))
+                    (lambda (si env arg* ...) b b* ...))))))
 
 (define (primitive? x)
     ; is x a primitve
@@ -49,43 +65,28 @@
     ; is expr a primitive call
     (and (pair? expr) (primitive? (car expr))))
 
-; cases for compiler
-(define (compile-program x)
-    ; initialize stack index - word-size so as not to 
-    ; overwrite return address
-    (emit-expr (- word-size) x)
-    (emit "ret"))
-
-(define (emit-expr si expr)
-    (cond
-        ((immediate? expr) (emit-immediate expr))
-        ((primcall? expr) (emit-primcall si expr))
-        (else (error "syntax error"))))
-
-(define (emit-primcall si expr)
+(define (emit-primcall si env expr)
     (let ((prim (car expr))
           (args (cdr expr)))
-        (apply (primitive-emitter prim) si args)))
+        (apply (primitive-emitter prim) si env args)))
 
-(define (emit-immediate expr)
-    (emit "movl $~a, %eax" (immediate-rep expr)))
 
-; add primitives
-(define-primitive (add1 si arg)
-    (emit-expr si arg)
+; unary primitives
+(define-primitive (add1 si env arg)
+    (emit-expr si env arg)
     (emit "addl $~a, %eax" (immediate-rep 1)))
 
-(define-primitive (sub1 si arg)
-    (emit-expr si arg)
+(define-primitive (sub1 si env arg)
+    (emit-expr si env arg)
     (emit "subl $~a, %eax" (immediate-rep 1)))
 
-(define-primitive (integer->char si arg)
-    (emit-expr si arg)
+(define-primitive (integer->char si env arg)
+    (emit-expr si env arg)
     (emit "shll $~a, %eax" (- char-shift fixnum-shift))
     (emit "orl $~a, %eax" char-tag))
 
-(define-primitive (char->integer si arg)
-    (emit-expr si arg)
+(define-primitive (char->integer si env arg)
+    (emit-expr si env arg)
     (emit "shrl $~a, %eax" (- char-shift fixnum-shift)))
 
 (define (zeroflag-to-bool)
@@ -99,87 +100,87 @@
     (emit "sall $~a, %eax" bool-shift)
     (emit "orl $~a, %eax" bool-tag))
 
-(define-primitive (zero? si arg)
-    (emit-expr si arg)
+(define-primitive (zero? si env arg)
+    (emit-expr si env arg)
     ; compare 0 to result and set zero flag if true
     (emit "cmpl $0, %eax")
     (zeroflag-to-bool))
 
-(define-primitive (null? si arg)
-    (emit-expr si arg)
+(define-primitive (null? si env arg)
+    (emit-expr si env arg)
     ; compare null value bits to result and set zero flag if true
     (emit "cmpl $~a, %eax" null-val)
     (zeroflag-to-bool))
 
-(define-primitive (not si arg)
+(define-primitive (not si env arg)
     ; return true only if arg is #f
-    (emit-expr si arg)
+    (emit-expr si env arg)
     ; compare immediate rep of #f to result
     ; set zero flag if true
     (emit "cmpl $~a, %eax" (immediate-rep #f))
     (zeroflag-to-bool))
 
-(define-primitive (integer? si arg)
-    (emit-expr si arg)
+(define-primitive (integer? si env arg)
+    (emit-expr si env arg)
     ; apply fixnum mask (1 bits fixnum-shift times)
     (emit "and $~s, %al" (- (ash 1 fixnum-shift) 1))
     (emit "cmp $~s, %al" #b00)
     (zeroflag-to-bool))
 
-(define-primitive (char? si arg)
-    (emit-expr si arg)
+(define-primitive (char? si env arg)
+    (emit-expr si env arg)
     ; apply fixnum mask (1 bits char-shift times)
     (emit "and $~s, %al" (- (ash 1 char-shift) 1))
     (emit "cmp $~s, %al" char-tag)
     (zeroflag-to-bool))
 
-(define-primitive (boolean? si arg)
-    (emit-expr si arg)
+(define-primitive (boolean? si env arg)
+    (emit-expr si env arg)
     ; apply fixnum mask (1 bits bool-shift times)
     (emit "and $~s, %al" (- (ash 1 bool-shift) 1))
     (emit "cmp $~s, %al" bool-tag)
     (zeroflag-to-bool))
 
 ; binary primitives
-(define-primitive (+ si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (+ si env arg1 arg2)
+    (emit-expr si env arg1)
     
     ; save result of arg1 in stack
     (emit "movl %eax, ~a(%rsp)" si)
 
     ; move stack index by a word so that 
     ; above is not overwritten
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
 
     ; add result from stack with arg2 result
     ; this works because of integer tag = b00
     (emit "addl ~a(%rsp), %eax" si))
 
-(define-primitive (- si arg1 arg2)
-    (emit-expr si arg2)
+(define-primitive (- si env arg1 arg2)
+    (emit-expr si env arg2)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg1)
+    (emit-expr (- si word-size) env arg1)
     (emit "subl ~a(%rsp), %eax" si))
 
-(define-primitive (* si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (* si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     ; remove b00 from int for one of the args
     (emit "shrl $~a, %eax" fixnum-shift)
     (emit "imull ~a(%rsp), %eax" si))
 
-(define-primitive (= si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (= si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     (emit "cmpl ~a(%rsp), %eax" si)
     (zeroflag-to-bool))
 
-(define-primitive (< si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (< si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     (emit "cmpl %eax, ~a(%rsp)" si)
     
     (emit "movl $0, %eax")
@@ -189,10 +190,10 @@
     (emit "sall $~a, %eax" bool-shift)
     (emit "orl $~a, %eax" bool-tag))
 
-(define-primitive (<= si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (<= si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     (emit "cmpl %eax, ~a(%rsp)" si)
     
     (emit "movl $0, %eax")
@@ -200,10 +201,10 @@
     (emit "sall $~a, %eax" bool-shift)
     (emit "orl $~a, %eax" bool-tag))
 
-(define-primitive (> si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (> si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     (emit "cmpl %eax, ~a(%rsp)" si)
     
     (emit "movl $0, %eax")
@@ -211,13 +212,19 @@
     (emit "sall $~a, %eax" bool-shift)
     (emit "orl $~a, %eax" bool-tag))
 
-(define-primitive (>= si arg1 arg2)
-    (emit-expr si arg1)
+(define-primitive (>= si env arg1 arg2)
+    (emit-expr si env arg1)
     (emit "movl %eax, ~a(%rsp)" si)
-    (emit-expr (- si word-size) arg2)
+    (emit-expr (- si word-size) env arg2)
     (emit "cmpl %eax, ~a(%rsp)" si)
     
     (emit "movl $0, %eax")
     (emit "setge %al")
     (emit "sall $~a, %eax" bool-shift)
     (emit "orl $~a, %eax" bool-tag))
+
+; variables and let
+(define (variable? x)
+    ; variable is any symbol that's not a primitive
+    (and (symbol? x) (not (primitive? x))))
+
